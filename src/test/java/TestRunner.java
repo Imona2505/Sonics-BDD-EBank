ca61a2dfe1a90315b5ae339810a7c04fce69dc08
MadinaMova <inogomovadina@gmail.com>
2023-01-02 19:59:39 -0500

    TestRunnerClasss

---
+++ src/test/java/TestRunner.java
@@ -0,0 +1,15 @@
+import io.cucumber.testng.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = {"src/test/java/features"},
+        tags = "@regression",
+        plugin = {"pretty","html:target/cucumber-reports.html","json:target/cucumber.json"}
+)
+
+
+
+public class TestRunner {
+
+}
