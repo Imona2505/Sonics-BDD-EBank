e450339d0506f02fd77d4a117aadd05212b00f85
Madina <madin.djalilova@gmail.com>
2023-01-02 20:06:13 -0500

    Hooks

---
+++ src/test/java/Utilities/Hooks.java
@@ -0,0 +1,35 @@
+package utilities;
+
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class Hooks {
+    public static WebDriver driver;
+    public static WebDriverWait wait;
+
+    @Before
+    public void setUp(){
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.setHeadless(true);
+
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+        wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+        driver.manage().window().maximize();
+    }
+    @After
+    public void tearDown(){
+
+        driver.quit();
+    }
+}
+
