26ca68758b46ba756c04f64782085aea64d31f5d
Munavvar <munavvar.balata@gmail.com>
2023-01-03 21:57:36 -0500

    outgoingWireTransfer

---
+++ src/test/java/PageObjects/OutgoingWireTransfer.java
@@ -0,0 +1,17 @@
+package PageObjects;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class OutgoingWireTransfer {
+    @When("User transfers money")
+    public void user_transfers_money() {
+
+    }
+    @Then("User successfully transfer money")
+    public void user_successfully_transfer_money() {
+
+    }
+
+
+}
